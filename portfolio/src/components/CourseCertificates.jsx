26f6c29a96f245941b6bd54feca603d1627e1412
Mohamed Shakin <shakincc02@gmail.com>
2026-03-12 18:19:09 +0400

    adjusted the spacings

--- portfolio/src/components/CourseCertificates.jsx
+++ portfolio/src/components/CourseCertificates.jsx
@@ -124,7 +124,7 @@ function CourseCertificates() {
   `}
       </style>
 
-      <div className="m-10 mx-5 mt-15 sm:mt-12 lg:mx-10 lg:mt-15 h-full flex flex-col justify-center">
+      <div className="m-10 mx-5 mt-15 sm:mt-12 lg:mx-10 lg:mt-15 h-full flex flex-col justify-center mb-20 sm:mb-0">
 
         <div className="all-certificates-content relative">
 

485c2c521895b8b539369ed5ef7e51a0e9ca3243
Mohamed Shakin <shakincc02@gmail.com>
2026-03-13 17:29:10 +0400

    updated the description

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -6,12 +6,11 @@ function About() {
             <section id="about" className="m-10 mx-5 mt-0 sm:mt-10  lg:mx-10 flex flex-col gap-14">
                 <div>
                     <p className="text-2xl lg:text-2xl font-normal text-justify">
-                        I'm Mohamed Shakin, passionate about building creative and practical digital solutions that solve real-world problems.
-                        With a background in Creative Computing, I've developed a solid foundation in front-end development and programming using HTML, CSS, JavaScript, and Python.
+                        I'm Mohamed Shakin, passionate about building creative and practical digital solutions that solve real-world problems. With a background in Creative Computing, I have developed a strong foundation in web development, software development, game development, and UI/UX design. I create responsive and modern websites using technologies such as HTML, CSS, JavaScript, Sass, Bootstrap, Tailwind, and React, along with backend tools like Node.js, Express.js, and MongoDB.
                         <br /><br />
-                        I enjoy exploring how design and technology intersect to create meaningful user experiences. Whether it's developing a website, prototyping an app, or automating a task, I approach every project with a problem-solving mindset and an eye for detail.
+                        I enjoy applying my skills to create meaningful user experiences. Whether it's developing a website, building a software application, designing an interactive game, or crafting intuitive interfaces, I approach every project with a problem-solving mindset and attention to detail.
                         <br /><br />
-                        I'm always looking for new challenges and opportunities to grow, especially those that allow me to collaborate, learn, and make a real impact through technology.
+                        I'm always looking for opportunities to grow and collaborate on projects that challenge me, learn new skills, and make a real impact through technology.
                     </p>
                 </div>
 
